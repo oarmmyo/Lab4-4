0dc324d5bd96792c85c804c0f0f8b289971f6398
Theeranai Nakpradit <9nongarm@gmail.com>
2020-08-26 11:40:46 +0000

    edit

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-
 int main(void) {
-  float L1, L2, Height, SumL, Area;
+  float L1, L2, Height, SumL, Area, Per;
   printf("Enter L1 : ");
   scanf("%f",&L1);
   printf("Enter L2 : ");
   scanf("%f",&L2);
   printf("Enter Height : ");
   scanf("%f",&Height);
-  SumL = L1+L2;
-  Area = 0.5*Height*SumL;
-  printf("Area  = %.2f",Area);
+  Per = ((L2-L1)/L2)*100;
+  printf("Percent = %.2f%%\n",Per);
+  if(Per >= 40){
+    SumL = L1+L2;
+    Area = 0.5*Height*SumL;
+    printf("Area  = %.2f",Area);
+  }else{
+    printf("This program will run when L2 is 40%% greater than L1");
+  }
   return 0;
 }
